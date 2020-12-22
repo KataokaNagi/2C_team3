b1c9139d30f3b03f25d6ad1c19f385c07f9922ab
99kenta <al18006@shibaura@it.ac.jp>
2020-12-23 02:25:05 +0900

    GUI追加

---
+++ MonsterHunter/src/application/BattleController.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class BattleController {
+
+    @FXML
+    private Label Attack_Log;
+
+    @FXML
+    private Label HP_Value;
+
+    @FXML
+    private Label Sharpmess_Value;
+
+    @FXML
+    private Button AttackButton;
+
+    @FXML
+    void OnAttackButton(ActionEvent event) {
+
+    }
+
+}
--- MonsterHunter/src/application/MenuController.java
+++ MonsterHunter/src/application/MenuController.java
@@ -1,5 +1,52 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
 public class MenuController {
-	
+
+    @FXML
+    private ComboBox<String> weapon_field;
+
+    @FXML
+    private ComboBox<String> armor_field;
+
+    @FXML
+    private ComboBox<String> monster_field;
+
+    @FXML
+    private Button StartButton;
+
+    @FXML
+    private Button EndButton;
+
+    @FXML
+    private Label Attack_value;
+
+    @FXML
+    private Label Defense_value;
+
+    @FXML
+    void OnEndButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnStartButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectArmor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectWeapon(ActionEvent event) {
+
+    }
+
 }
