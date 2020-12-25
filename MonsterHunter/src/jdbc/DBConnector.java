03a4c2ad409b6674104a8d82bca62e33e6c9db81
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-26 02:19:37 +0900

    DBConnectorのコメントの修正・追加など

--- MonsterHunter/src/jdbc/DBConnector.java
+++ MonsterHunter/src/jdbc/DBConnector.java
@@ -1,5 +1,5 @@
 /**
-* @file      DBConnector
+* @file      DBConnector.java
 * @brief     DBへの接続を行うDAOの抽象クラス
 * @note      高度情報演習2C 後半 木村教授担当分 Team3
 * @auther    AL18036 Kataoka Nagi
@@ -14,16 +14,23 @@
 package jdbc;
 
 import java.sql.*;
-import java.util.ArrayList;
 
+/**
+ * @class DBConnector
+ * @brief DBへの接続
+ */
 abstract class DBConnector {
   private static final String RDB_DRIVE = "com.mysql.jdbc.Driver";
   private static final String URL = "jdbc:mysql://localhost/mybookdb";
   private static final String USER = "group-b";
   private static final String PASSWD = "monhan";
 
-  // データベース接続を行うメソッド
-  // データベース接続用定義を基にデータベースへ接続し、戻り値としてコネクション情報を返す
+  /**
+   * @fn getConnection
+   * @brief データベース接続を行うメソッド
+   * @param[in] void
+   * @return コネクション情報
+   */
   public static Connection getConnection() {
     try {
       Class.forName(RDB_DRIVE);
