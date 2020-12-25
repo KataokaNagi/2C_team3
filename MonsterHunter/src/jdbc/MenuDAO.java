8ce0a27fda249ad670e2f32d91efa65ca5c7f59b
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-26 02:11:56 +0900

    DAOのヘッダコメントを追加, extends

--- MonsterHunter/src/jdbc/MenuDAO.java
+++ MonsterHunter/src/jdbc/MenuDAO.java
@@ -1,5 +1,18 @@
+/**
+* @file      MenuDAO.java
+* @brief     メニュー画面で用いるデータアクセスオブジェクト
+* @note      高度情報演習2C 後半 木村教授担当分 Team3
+* @auther    AL18036 Kataoka Nagi
+* @date      2020-12-26 02:05:43
+* $Version   1.0
+* $Revision  1.0
+* @par       変更点　：記事から引用し，メソッド名を変更
+* @par       変更予定：
+* @see       https://www.kenschool.jp/blog/?p=1644
+ */
+
 package jdbc;
 
-public class MenuDAO {
+public class MenuDAO extends DBConnector {
 
 }
