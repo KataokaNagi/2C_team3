b3432c1bfdb5062afdbb052fc2026c1bf91553d9
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-26 22:41:16 +0900

    MenuDAO.javaから、BattleDAOと共通する要素をDataAccessObject.javaへ

---
+++ MonsterHunter/src/jdbc/DataAccessObject.java
@@ -0,0 +1,107 @@
+/**
+* @file      DataAccessObject.java
+* @brief     DAOの汎用部分を実装する抽象クラス
+* @note      高度情報演習2C 後半 木村教授担当分 Team3
+* @auther    AL18036 Kataoka Nagi
+* @date      2020-12-26 22:36:34
+* $Version   1.0
+* $Revision  1.0
+* @par       変更点　：MenuDAO.javaから、BattleDAOと共通する要素を取り出し
+* @see       https://www.kenschool.jp/blog/?p=1644
+ */
+
+package jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * @class DataAccessObject
+ * @brief DAOの汎用部分を実装する抽象クラス
+ */
+abstract class DataAccessObject extends DBConnector {
+
+  /**
+   * @fn selectColumn
+   * @brief カラム内のフィールドを全て返す
+   * @param[in] columnName: 検索したいカラムの名前
+   * @param[in] tableName: 検索したいカラムが存在するテーブルの名前
+   * @param[in] primaryKeyName: 検索したいカラムの主キーの名前
+   * @return 指定されたカラムの全フィールドのオブジェクトリスト
+   */
+  protected ArrayList<String> selectColumn(String columnName, String tableName, String primaryKeyName) {
+    Connection connection = null; // ! DBコネクション
+    Statement statement = null; // ! SQLステートメント
+    ArrayList<String> rtnColumnList = new ArrayList<String>();
+
+    // SQL文の作成
+    // 主キーで整列することでSELECT文の順序を保証し、
+    // 武器名と武器スキルを同じインデックスで取得できるようにする
+    String sql = "";
+    sql += "SELECT " + columnName;
+    sql += " FROM " + tableName;
+    sql += "ORDER BY " + primaryKeyName;
+
+    try {
+      // DBに接続
+      connection = DBConnector.getConnection();
+      statement = connection.createStatement();
+
+      // SQL文発行
+      ResultSet rs = statement.executeQuery(sql);
+
+      // 検索結果をArrayListに格納
+      while (rs.next()) {
+        rtnColumnList.add(rs.getString("isbn"));
+      }
+
+      // 例外処理
+    } catch (SQLException e) {
+      System.out.println("Error: Failed to select column" + e);
+
+      // 後処理
+    } finally {
+      this.closeResources(statement, connection);
+    }
+
+    return rtnColumnList;
+  }
+
+  /**
+   * @fn toIntegerList
+   * @brief StringのリストをIntegerのリストに変換
+   * @param[in] strList
+   * @return intList
+   */
+  protected ArrayList<Integer> toIntegerList(ArrayList<String> strList) {
+    ArrayList<Integer> intList = new ArrayList<Integer>();
+    for (String str : strList) {
+      intList.add(Integer.parseInt(str));
+    }
+    return intList;
+  }
+
+  /**
+   * @fn closeResources
+   * @brief ステートメントとコネクションをこの順番にクローズする
+   * @param[in] statement: DBのステートメント
+   * @param[in] connection: DBの接続情報
+   */
+  protected void closeResources(Statement statement, Connection connection) {
+    // statementの後処理
+    if (statement != null) {
+      try {
+        statement.close();
+      } catch (SQLException ignore) {
+      }
+    }
+
+    // connectionの後処理
+    if (connection != null) {
+      try {
+        connection.close();
+      } catch (SQLException ignore) {
+      }
+    }
+  }
+}
--- MonsterHunter/src/jdbc/MenuDAO.java
+++ MonsterHunter/src/jdbc/MenuDAO.java
@@ -3,10 +3,10 @@
 * @brief     メニュー画面で用いるデータアクセスオブジェクト
 * @note      高度情報演習2C 後半 木村教授担当分 Team3
 * @auther    AL18036 Kataoka Nagi
-* @date      2020-12-26 05:39:40
-* $Version   1.0
+* @date      2020-12-26 22:30:14
+* $Version   1.1
 * $Revision  1.0
-* @par       EDIT：記事から引用し，メソッド名を変更
+* @par       EDIT：汎用的なコードをDataAccessObject.javaに移動
 * @par       TODO：クエスト後に検索が不要になるようにリストに退避
 * @par       TODO：ConnectionとStatementのタイミングが頻繁すぎる気がする
 * @par       TODO：Debianへの対応
@@ -15,14 +15,13 @@
 
 package jdbc;
 
-import java.sql.*;
 import java.util.ArrayList;
 
 /**
  * @class MenuDAO
  * @brief メニュー画面で用いるデータアクセスオブジェクト
  */
-public class MenuDAO extends DBConnector {
+public class MenuDAO extends DataAccessObject {
 
   /**
    * @fn selectAllWeaponName
@@ -91,89 +90,5 @@ public class MenuDAO extends DBConnector {
     String tableName = "armor";
     String primaryKeyName = "armor_id";
     return toIntegerList(selectColumn(columnName, tableName, primaryKeyName));
-  }
-
-  /**
-   * @fn selectColumn
-   * @brief カラム内のフィールドを全て返す
-   * @param[in] columnName: 検索したいカラムの名前
-   * @param[in] tableName: 検索したいカラムが存在するテーブルの名前
-   * @param[in] primaryKeyName: 検索したいカラムの主キーの名前
-   * @return 指定されたカラムの全フィールドのオブジェクトリスト
-   */
-  private ArrayList<String> selectColumn(String columnName, String tableName, String primaryKeyName) {
-    Connection connection = null; // ! DBコネクション
-    Statement statement = null; // ! SQLステートメント
-    ArrayList<String> rtnColumnList = new ArrayList<String>();
-
-    // SQL文の作成
-    // 主キーで整列することでSELECT文の順序を保証し、
-    // 武器名と武器スキルを同じインデックスで取得できるようにする
-    String sql = "";
-    sql += "SELECT " + columnName;
-    sql += " FROM " + tableName;
-    sql += "ORDER BY " + primaryKeyName;
-
-    try {
-      // DBに接続
-      connection = DBConnector.getConnection();
-      statement = connection.createStatement();
-
-      // SQL文発行
-      ResultSet rs = statement.executeQuery(sql);
-
-      // 検索結果をArrayListに格納
-      while (rs.next()) {
-        rtnColumnList.add(rs.getString("isbn"));
-      }
-
-      // 例外処理
-    } catch (SQLException e) {
-      System.out.println("Error: Failed to select column" + e);
-
-      // 後処理
-    } finally {
-      closeResources(statement, connection);
-    }
-
-    return rtnColumnList;
-  }
-
-  /**
-   * @fn toIntegerList
-   * @brief StringのリストをIntegerのリストに変換
-   * @param[in] strList
-   * @return intList
-   */
-  private ArrayList<Integer> toIntegerList(ArrayList<String> strList) {
-    ArrayList<Integer> intList = new ArrayList<Integer>();
-    for (String str : strList) {
-      intList.add(Integer.parseInt(str));
-    }
-    return intList;
-  }
-
-  /**
-   * @fn closeResources
-   * @brief ステートメントとコネクションをこの順番にクローズする
-   * @param[in] statement: DBのステートメント
-   * @param[in] connection: DBの接続情報
-   */
-  private void closeResources(Statement statement, Connection connection) {
-    // statementの後処理
-    if (statement != null) {
-      try {
-        statement.close();
-      } catch (SQLException ignore) {
-      }
-    }
-
-    // connectionの後処理
-    if (connection != null) {
-      try {
-        connection.close();
-      } catch (SQLException ignore) {
-      }
-    }
   }
 }
