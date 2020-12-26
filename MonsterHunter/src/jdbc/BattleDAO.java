14e87ac534d1f28792544aab59e78fc5343e7f22
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-27 03:59:17 +0900

    BattleDAOの抽象化，メンバの初期化

--- MonsterHunter/src/jdbc/BattleDAO.java
+++ MonsterHunter/src/jdbc/BattleDAO.java
@@ -3,16 +3,28 @@
 * @brief     バトル画面で用いるデータアクセスオブジェクト
 * @note      高度情報演習2C 後半 木村教授担当分 Team3
 * @auther    AL18036 Kataoka Nagi
-* @date      2020-12-26 02:05:43
+* @date      2020-12-26 22:43:31
 * $Version   1.0
-* $Revision  1.0
-* @par       変更点　：記事から引用し，メソッド名を変更
-* @par       変更予定：
+* $Revision  1.1
+* @par       変更点　：メンバの追加と初期化
+* @par       変更点　：クラスの抽象化（非正規化のテーブルごとにクラスを作成予定）
 * @see       https://www.kenschool.jp/blog/?p=1644
- */
+*/
 
 package jdbc;
 
-public class BattleDAO extends DBConnector {
+abstract class BattleDAO extends DataAccessObject {
+  private String weaponName;
+  private String armorName;
+  private String monsterName;
 
+  /**
+   * @fn BattleDAO
+   * @brief メンバの初期化（メニュー画面で選択した項目名）
+   */
+  public BattleDAO(String weaponName, String armorName, String monsterName) {
+    this.weaponName = weaponName;
+    this.armorName = armorName;
+    this.monsterName = monsterName;
+  }
 }
