01e805fd0c86e87cdcace1d087b439572cec39e4
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-28 01:29:45 +0900

    ButtleDAOの子クラス群を新規作成（ヘッダコメントのみ）

---
+++ MonsterHunter/src/jdbc/MonsterMainHpDAO.java
@@ -0,0 +1,17 @@
+/**
+* @file      MonsterMainHpDAO.java
+* @brief     「モンスター総合体力」テーブルのDAO
+* @note      高度情報演習2C 後半 木村教授担当分 Team3
+* @auther    AL18036 Kataoka Nagi
+* @date      2020-12-28 00:54:39
+* $Version   1.0
+* $Revision  1.0
+* @par       ADD: 新規作成
+* @see       https://www.kenschool.jp/blog/?p=1644
+*/
+
+package jdbc;
+
+public class MonsterMainHpDAO extends BattleDAO {
+
+}
---
+++ MonsterHunter/src/jdbc/PlayerHpDAO.java
@@ -0,0 +1,17 @@
+/**
+* @file      PlayerHpDAO.java
+* @brief     「プレイヤー体力」テーブル
+* @note      高度情報演習2C 後半 木村教授担当分 Team3
+* @auther    AL18036 Kataoka Nagi
+* @date      2020-12-28 00:54:39
+* $Version   1.0
+* $Revision  1.0
+* @par       ADD: 新規作成
+* @see       https://www.kenschool.jp/blog/?p=1644
+*/
+
+package jdbc;
+
+public class PlayerHpDAO extends BattleDAO extends BattleDAO {
+  
+}
