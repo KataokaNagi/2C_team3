5909fae22169159ceb35beb17f060e1f3202acf1
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-28 06:17:40 +0900

    PlayerHpDAOのcreate, drop, select

--- MonsterHunter/src/jdbc/PlayerHpDAO.java
+++ MonsterHunter/src/jdbc/PlayerHpDAO.java
@@ -6,12 +6,52 @@
 * @date      2020-12-28 00:54:39
 * $Version   1.0
 * $Revision  1.0
-* @par       ADD: 新規作成
+* @par       ADD: 仮組の作成
 * @see       https://www.kenschool.jp/blog/?p=1644
 */
 
 package jdbc;
 
-public class PlayerHpDAO extends BattleDAO extends BattleDAO {
-  
-}
+/**
+ * @class PlayerHpDAO
+ * @brief 「プレイヤー体力」テーブルのDAO
+ */
+public class PlayerHpDAO extends BattleDAO {
+
+  /**
+   * @fn PlayerHpDAO
+   * @brief 「プレイヤー体力」テーブルの作成を行うコンストラクタ
+   */
+  public PlayerHpDAO(String weaponName, String armorName, String monsterName) {
+    super(weaponName, armorName, monsterName);
+    createPlayerHpTable();
+  }
+
+  /**
+   * @fn createPlayerHpTable
+   * @brief 「プレイヤー体力」テーブルの作成
+   */
+  private void createPlayerHpTable() {
+    // TODO
+  }
+
+  /**
+   * @fn dropPlayerHpTable
+   * @brief 「プレイヤー体力」テーブルの削除
+   */
+  public void dropPlayerHpTable() {
+    // TODO
+  }
+
+  /**
+   * @fn selectFirstPlayerHp
+   * @brief 「プレイヤー体力」テーブルの、プレイヤー体力カラムの先頭のフィールドを返す
+   * @return プレイヤー体力の先頭のフィールド
+   */
+  public String selectFirstPlayerHp() {
+    String columnName = "player_hp";
+    String tableName = "player_hp_table";
+    String primaryKeyColumnName = "player_code";
+    return selectFirstField(columnName, tableName, primaryKeyColumnName);
+  }
+
