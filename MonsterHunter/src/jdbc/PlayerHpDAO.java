4b379159ed99985883083f0fc7ac71163497a572
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-28 06:33:11 +0900

    PlayerHpDAOのupdate

--- MonsterHunter/src/jdbc/PlayerHpDAO.java
+++ MonsterHunter/src/jdbc/PlayerHpDAO.java
@@ -3,9 +3,9 @@
 * @brief     「プレイヤー体力」テーブル
 * @note      高度情報演習2C 後半 木村教授担当分 Team3
 * @auther    AL18036 Kataoka Nagi
-* @date      2020-12-28 00:54:39
+* @date      2020-12-28 06:31:02
 * $Version   1.0
-* $Revision  1.0
+* $Revision  1.1
 * @par       ADD: 仮組の作成
 * @see       https://www.kenschool.jp/blog/?p=1644
 */
@@ -48,11 +48,21 @@ public class PlayerHpDAO extends BattleDAO {
    * @brief 「プレイヤー体力」テーブルの、プレイヤー体力カラムの先頭のフィールドを返す
    * @return プレイヤー体力の先頭のフィールド
    */
-  public String selectFirstPlayerHp() {
+  public int selectFirstPlayerHp() {
     String columnName = "player_hp";
     String tableName = "player_hp_table";
     String primaryKeyColumnName = "player_code";
-    return selectFirstField(columnName, tableName, primaryKeyColumnName);
+    return Integer.parseInt(selectFirstField(columnName, tableName, primaryKeyColumnName));
   }
-  
+
+  /**
+   * @fn updateFirstPlayerHp
+   * @brief 「プレイヤー体力」テーブルの、プレイヤー体力カラムの先頭のフィールドを更新
+   */
+  public void updateFirstPlayerHp() {
+    String columnName = "player_hp";
+    String tableName = "player_hp_table";
+    String primaryKeyColumnName = "player_code";
+    this.updateFirstField(columnName, tableName, primaryKeyColumnName);
+  }
 }
