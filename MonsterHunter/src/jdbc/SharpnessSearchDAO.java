9e5252d28fa581f594bbb33ef271e216d3ae55c4
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-28 03:13:00 +0900

    SharpnessSearchDAOの仮組、selectの完成

--- MonsterHunter/src/jdbc/SharpnessSearchDAO.java
+++ MonsterHunter/src/jdbc/SharpnessSearchDAO.java
@@ -3,15 +3,81 @@
 * @brief     「切れ味色ごと検索」テーブルのDAO
 * @note      高度情報演習2C 後半 木村教授担当分 Team3
 * @auther    AL18036 Kataoka Nagi
-* @date      2020-12-28 00:54:39
+* @date      2020-12-28 03:02:34
 * $Version   1.0
-* $Revision  1.0
-* @par       ADD: 新規作成
+* $Revision  1.1
+* @par       ADD: 仮組の作成
 * @see       https://www.kenschool.jp/blog/?p=1644
 */
 
 package jdbc;
 
+import java.util.ArrayList;
+
+/**
+ * @class SharpnessSearchDAO
+ * @brief 「切れ味色ごと検索」テーブルのDAO
+ */
 public class SharpnessSearchDAO extends BattleDAO {
 
+  /**
+   * @fn SharpnessSearchDAO
+   * @brief 切れ味色ごと検索テーブルの作成を行うコンストラクタ
+   */
+  public SharpnessSearchDAO(String weaponName, String armorName, String monsterName) {
+    super(weaponName, armorName, monsterName);
+    createSharpnessSearchTable();
+  }
+
+  /**
+   * @fn createSharpnessSearchTable
+   * @brief 切れ味色ごと検索テーブルの作成
+   */
+  private void createSharpnessSearchTable() {
+    // TODO
+  }
+
+  /**
+   * @fn dropSharpnessSearchTable
+   * @brief 切れ味色ごと検索テーブルの削除
+   */
+  public void dropSharpnessSearchTable() {
+    // TODO
+  }
+
+  /**
+   * @fn selectAllSharpnessColorName
+   * @brief 切れ味色ごと検索テーブルの、切れ味色名カラムの全てのフィールドを返す
+   * @return 全切れ味色名のリスト
+   */
+  public ArrayList<String> selectAllSharpnessColorName() {
+    String columnName = "sharpness_color_name";
+    String tableName = "sharpness_search";
+    String primaryKeyColumnName = "sharpness_color_code";
+    return selectColumn(columnName, tableName, primaryKeyColumnName);
+  }
+
+  /**
+   * @fn selectAllSharpnessGaugeAmount
+   * @brief 切れ味色ごと検索テーブルの切れ味ゲージ量名カラムの全てのフィールドを返す
+   * @return 全切れ味ゲージ量名のリスト
+   */
+  public ArrayList<String> selectAllSharpnessGaugeAmount() {
+    String columnName = "sharpness_gauge_amount";
+    String tableName = "sharpness_search";
+    String primaryKeyColumnName = "sharpness_color_code";
+    return selectColumn(columnName, tableName, primaryKeyColumnName);
+  }
+
+  /**
+   * @fn selectAllSharpnessIncreasingAttackValueRate
+   * @brief 切れ味色ごと検索テーブルの切れ味攻撃力倍率カラムの全てのフィールドを返す
+   * @return 全切れ味攻撃力倍率のリスト
+   */
+  public ArrayList<String> selectAllSharpnessIncreasingAttackValueRate() {
+    String columnName = "sharpness_increasing_attack_value_rate";
+    String tableName = "sharpness_search";
+    String primaryKeyColumnName = "sharpness_color_code";
+    return selectColumn(columnName, tableName, primaryKeyColumnName);
+  }
 }
