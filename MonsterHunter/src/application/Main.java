c7a87b275862a55331848a256ca21cafa9c6b384
99kenta <al18006@shibaura@it.ac.jp>
2020-12-28 21:55:21 +0900

    メニュー画面処理更新(MenuDAOとのやり取り)

--- MonsterHunter/src/application/Main.java
+++ MonsterHunter/src/application/Main.java
@@ -9,17 +9,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 
 	private static Stage stage;
+
 	@Override
 	public void start(Stage stage) {
 		try {
 			Main.stage = stage;
 			changeView("Menu.fxml");
 			Main.stage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -28,11 +28,11 @@ public class Main extends Application {
 		launch(args);
 	}
 
-	public void changeView(String fxml) {
-        try {
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxml))));
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	public void changeView(String fxml) { // シーン切り替え用の関数
+		try {
+			stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxml))));
+		} catch (IOException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
--- MonsterHunter/src/application/MenuController.java
+++ MonsterHunter/src/application/MenuController.java
@@ -1,77 +1,99 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import jdbc.MenuDAO;
 
 public class MenuController {
 
-    @FXML
-    private ComboBox<String> weapon_field;
+	//MenuDAOからArrayListを取得
+//	MenuDAO MenuDao = new MenuDAO();
+//	ArrayList<String> weaponArry = MenuDao.selectAllWeaponName();
+//	ArrayList<String> armorArry = MenuDao.selectAllArmorName();
+//	ArrayList<String> monsterArry = MenuDao.selectAllMonsterName();
+//	ArrayList<Integer> AttackArry = MenuDao.selectAllWeaponAttackVal();
+//	ArrayList<Integer> DeffenseArry = MenuDao.selectAllArmorDiffenceVal();
 
-    @FXML
-    private ComboBox<String> armor_field;
-
-    @FXML
-    private ComboBox<String> monster_field;
-
-    @FXML
-    private Button StartButton;
-
-    @FXML
-    private Button EndButton;
-
-    @FXML
-    private Label Attack_value;
-
-    @FXML
-    private Label Defense_value;
-
-    @FXML
-    private Label Error;
+	//テスト用
+	ArrayList<String> weaponArry = new ArrayList<String>(Arrays.asList("あ","い","う","え","お"));
+	ArrayList<String> armorArry = new ArrayList<String>(Arrays.asList("か","き","く","け","こ"));
+	ArrayList<String> monsterArry = new ArrayList<String>(Arrays.asList("さ","し","す","せ","そ"));
+	ArrayList<Integer> AttackArry = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+	ArrayList<Integer> DeffenseArry = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
 
 
-    @FXML
-    void OnEndButton(ActionEvent event) {
-    	System.exit(0);
-    }
+	@FXML
+	private ComboBox<String> weapon_field;
 
-    @FXML
-    void OnStartButton(ActionEvent event) {
-    	if(weapon_field.getValue()==null || armor_field.getValue()==null || monster_field==null) {
-    		Error.setVisible(true);
-    		return;
-    	}
-    	new Main().changeView("Battle.fxml");
-    }
+	@FXML
+	private ComboBox<String> armor_field;
 
-    @FXML
-    void selectArmor(ActionEvent event) {
+	@FXML
+	private ComboBox<String> monster_field;
 
-    }
+	@FXML
+	private Button StartButton;
 
-    @FXML
-    void selectWeapon(ActionEvent event) {
-    	if(armor_field.getValue()==null)
-    		return;
-    	System.out.println("ok");
-    }
+	@FXML
+	private Button EndButton;
 
-    @FXML
-    void initialize() {
-    	for(int i=0;i<10;++i) {
-    		weapon_field.getItems().add(""+i);
-    	}
-    	for(int i = 0;i<10;++i) {
-    		armor_field.getItems().add(""+i);
-    	}
-    	for(int i=0;i<10;++i) {
-    		monster_field.getItems().add(""+i);
-    	}
+	@FXML
+	private Label Attack_value;
 
-    	Error.setVisible(false);
-    }
+	@FXML
+	private Label Defense_value;
+
+	@FXML
+	private Label Error;
+
+	@FXML
+	void OnEndButton(ActionEvent event) { //終了ボタン
+		System.exit(0);
+	}
+
+	@FXML
+	void OnStartButton(ActionEvent event) { // スタートボタン
+		if (weapon_field.getValue() == null || armor_field.getValue() == null || monster_field == null) { // 武器防具モンスターどれか一つでも選択されていない場合
+			Error.setVisible(true);
+			return;
+		}
+		new Main().changeView("Battle.fxml");// バトル画面へ遷移
+	}
+
+	@FXML
+	void selectArmor(ActionEvent event) {
+		int n = armorArry.indexOf(armor_field.getValue());
+		if(n==-1)
+			return;
+		Defense_value.setText("防御力："+DeffenseArry.get(n));
+	}
+
+	@FXML
+	void selectWeapon(ActionEvent event) {
+		int n = weaponArry.indexOf(weapon_field.getValue());
+		if(n==-1)
+			return;
+		Attack_value.setText("攻撃力："+AttackArry.get(n));
+	}
+
+	@FXML
+	void initialize() {
+
+		// ComboBoxにそれぞれの値をセット
+		weapon_field.getItems().setAll(weaponArry);
+		armor_field.getItems().setAll(armorArry);
+		monster_field.getItems().setAll(monsterArry);
+
+
+		Error.setVisible(false);
+	}
+
+
 
 }
