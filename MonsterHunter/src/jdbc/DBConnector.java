3cbb09a53be720d530562d1ccbd42bab59634c41
KataokaNagi <al18036@shibaura-it.ac.jp>
2020-12-30 17:43:20 +0900

    DBConnector：MySQLからPostgreSQLに変更

--- MonsterHunter/src/jdbc/DBConnector.java
+++ MonsterHunter/src/jdbc/DBConnector.java
@@ -3,12 +3,12 @@
 * @brief     DBへの接続を行うDAOの抽象クラス
 * @note      高度情報演習2C 後半 木村教授担当分 Team3
 * @auther    AL18036 Kataoka Nagi
-* @date      2020-12-26 01:58:29
-* $Version   1.0
+* @date      2020-12-30 17:41:51
+* $Version   1.1
 * $Revision  1.0
-* @par       変更点　：記事から引用し，USERとPASSWDを変更
+* @par       変更点　：MySQLからPostgreSQLに変更
 * @par       変更予定：Debianで使えるように編集する必要があるかも
-* @see       https://www.kenschool.jp/blog/?p=1644
+* @see       https://qiita.com/wb773/items/27dc0e77a4c8b035d6fc
  */
 
 package jdbc;
@@ -20,10 +20,10 @@ import java.sql.*;
  * @brief DBへの接続
  */
 abstract class DBConnector {
-  private static final String RDB_DRIVE = "com.mysql.jdbc.Driver";
-  private static final String URL = "jdbc:mysql://localhost/mybookdb";
-  private static final String USER = "group-b";
-  private static final String PASSWD = "monhan";
+  private static final String RDB_DRIVE = "org.postgresql.Driver";
+  private static final String URL = "jdbc:postgresql://localhost:5432/monsterhunter_db";
+  private static final String USER = "group_b";
+  private static final String PASSWD = "bond";
 
   /**
    * @fn getConnection
