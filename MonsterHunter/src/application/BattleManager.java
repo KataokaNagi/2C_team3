b86692359f1c2315bc426ed7261602fce53197c7
stsubasa0804 <stsubasa0804@icloud.com>
2020-12-31 15:00:25 +0900

    DAOからの参照追加

--- MonsterHunter/src/application/BattleManager.java
+++ MonsterHunter/src/application/BattleManager.java
@@ -2,6 +2,9 @@ package application;
 import java.util.ArrayList;
 import java.util.Random;
 
+import jdbc.MonsterPartsHpDAO;
+import jdbc.PlayerHpDAO;
+
 public class BattleManager {
 	//*テーブルから取得
 	int monster_id = 0;//*
@@ -12,7 +15,10 @@ public class BattleManager {
 
 	Random rnd = new Random();
 
-	public void PlayerAttack(int Parts_id) {
+	PlayerHpDAO playerHpDAO;
+
+	public int PlayerAttack(int Parts_id) {
+		MonsterPartsHpDAO monsterPartsHpDAO;
 		int HP_monster = 0;//*
 		int hardness = 0;//*
 		int attack_player = 0;//*
@@ -26,28 +32,34 @@ public class BattleManager {
 		}
 
 		float damage = attack_player *( (float)hardness /100) * affinityCorrection * sharpnessCorrection + ElementDamage_PlayerAttack();
-		HP_monster -= damage;
+		HP_monster -= (int)damage;
 		SharpnessDecrease(sharpness);
 		if (HP_monster <= 0) {
 			//シーン遷移(終了)
+			return -1;
 		}
 		else {
 			//HP_monsterを更新
-			MonsterAttack();
+			return (int)damage;
 		}
 	}
 
-	void MonsterAttack() {
-		int HP_player = 0;//*
+	public int MonsterAttack() {
+		int HP_player = playerHpDAO.selectFirstPlayerHp();//*
 		int deffence_player = 0;//*
 		int attackAction =  rnd.nextInt(10);//攻撃種類の決定
 		int attack_monster = 0;//*
 		int element = 0;//*
 
 		float damage = attack_monster *(80/(float)(80 + deffence_player)) * ElementCompatibilityCalculation_EnemyAttack(element);
-		HP_player -= damage;
+		HP_player -= (int)damage;
+		playerHpDAO.updateFirstPlayerHp(HP_player);
 		if (HP_player <= 0) {
 			//シーン遷移(終了)
+			return -1;
+		}
+		else {
+			return (int)damage;
 		}
 	}
 
@@ -67,17 +79,7 @@ public class BattleManager {
 		return result;
 	}
 
-	void AddLog(String addText) {
-		log.add(addText + "\n");
-		for(int i = log.size();i >= 0; --i) {
 
-		}
-		try {
-            Thread.sleep(1000);//1秒待機
-        } catch(InterruptedException e){
-            e.printStackTrace();
-        }
-	}
 
 	int SharpnessDecrease(int sharpness) {
 		sharpness -= 1;
